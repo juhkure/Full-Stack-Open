1fecdc372fc3a93b652872657c8a677843fa9154
juhkure <johannes.pyykonen@helsinki.fi>
2024-12-30 23:39:40 +0200

    add 4.15

--- osa4/app.js
+++ osa4/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
@@ -31,6 +32,7 @@ app.use(middleware.requestLogger)
 
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
